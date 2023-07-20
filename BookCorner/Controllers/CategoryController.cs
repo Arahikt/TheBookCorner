8357ca3b3b1b790c7ee544ebf31d8a6ff4365384
Arahik Torosian <arahikt@gmail.com>
2023-07-19 17:03:40 -0700

    Category Model & Controller added.

---
+++ BookCorner/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BookCorner.Data;
+using BookCorner.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BookCorner.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
+
