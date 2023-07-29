3f0d139c3ddbbb69891bd62419948d489ff8650e
Arahik Torosian <arahikt@gmail.com>
2023-07-29 16:35:52 -0700

    Added  ImageUrl and migration.

--- BookCorner.Models/Product.cs
+++ BookCorner.Models/Product.cs
@@ -41,5 +41,6 @@ namespace BookCorner.Models
         public int CategoryId  { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
